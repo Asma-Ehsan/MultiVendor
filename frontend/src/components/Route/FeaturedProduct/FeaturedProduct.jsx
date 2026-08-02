8fba8dcc97c4e7eb5a8396de9883a816d5978f43
Asma-Ehsan <asmaehsan212@gmail.com>
2026-08-03 00:10:36 +0500

    Update FeaturedProduct component to use allProducts from Redux state

--- frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
+++ frontend/src/components/Route/FeaturedProduct/FeaturedProduct.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import ProductCard from "../ProductCard/ProductCard";
 
 const FeaturedProduct = () => {
-  const{ products } = useSelector((state) => state.products);
+  const{ allProducts } = useSelector((state) => state.products);
   return (
     <div>
       <div className={`${styles.section}`}>
@@ -13,7 +13,7 @@ const FeaturedProduct = () => {
         </div>
         <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12 border-0">
             {
-                products && products.map((i, index) => <ProductCard data={i} key={index}/>)
+                allProducts && allProducts.map((i, index) => <ProductCard data={i} key={index}/>)
             }
         </div>
       </div>

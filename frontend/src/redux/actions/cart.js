991c21f8183965e288167a7a0df6071fc2c5f0a7
Asma-Ehsan <asmaehsan212@gmail.com>
2026-08-07 21:30:29 +0500

    added conmments for understanding

--- frontend/src/redux/actions/cart.js
+++ frontend/src/redux/actions/cart.js
@@ -5,9 +5,10 @@
 export const addToCart = (data) => async (dispatch, getState) => {
     dispatch({
         type: "addToCart",
-        payload: data,
+        payload: data, //data is the product's data which is added in to the cart
     });
 
+    // each time product is added to the cart, we will store the cart in local storage so that it can be retrieved on the initialState of the cartReducer
     localStorage.setItem("cartItems", JSON.stringify(getState().cart.cart));
     return data;
 }
--- frontend/src/redux/reducers/cart.js
+++ frontend/src/redux/reducers/cart.js
@@ -1,5 +1,6 @@
 import {createReducer} from "@reduxjs/toolkit";
 
+//it will check if there is any cart items in local storage or not, if yes then it will return the cart items from local storage(which is stired in actions) otherwise it will return an empty array
 const initialState = {
     cart: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")): [],
 }
@@ -8,6 +9,7 @@ export const cartReducer = createReducer(initialState, (builder) => {
     builder
     .addCase("addToCart", (state, action) => {
         const item = action.payload;
+        // check if the item is already in the cart or not
         const isItemExist = state.cart.find((i) => i._id === item._id); 
         if(isItemExist){
             return {

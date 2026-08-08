bac68c106c2758c4b29411045deb5d62ef23fb84
Asma-Ehsan <asmaehsan212@gmail.com>
2026-08-09 00:21:46 +0500

    added wishlist reducer

--- frontend/src/redux/stores.js
+++ frontend/src/redux/stores.js
@@ -4,6 +4,7 @@ import { sellerReducer } from "./reducers/seller";
 import { productReducer } from "./reducers/product.js";
 import { eventReducer } from "./reducers/event";
 import { cartReducer } from "./reducers/cart.js";
+import { wishlistReducer } from "./reducers/wishlist.js";
 
 const Store = configureStore({
   reducer: {
@@ -11,7 +12,8 @@ const Store = configureStore({
     seller: sellerReducer,
     products: productReducer,
     events: eventReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    wishlist: wishlistReducer,
   },
 });
 

1a1320d7d773cb77a8ede28a82dc10f32b65a85d
Asma-Ehsan <asmaehsan212@gmail.com>
2026-08-09 00:28:23 +0500

    created "add to wishlist" and "remove from wishlist" actions in redux

---
+++ frontend/src/redux/actions/wishlist.js
@@ -0,0 +1,21 @@
+// add to wishlist
+export const addToWishlist = (data) => async (dispatch, getState) => {
+    dispatch({
+        type: "addToWishlistt",
+        payload: data, //data is the product's data which is added in to the cart
+    });
+
+    // each time product is added to the cart, we will store the cart in local storage so that it can be retrieved on the initialState of the cartReducer
+    localStorage.setItem("wishlistItems", JSON.stringify(getState().wishlist.wishlist));
+    return data;
+}
+
+//remove from wishlist
+export const removeFromWishlist = (data) => async (dispatch, getState) => {
+    dispatch({
+        type: "removeFromWishlist",
+        payload: data._id,
+    });
+    localStorage.setItem("wishlistItems", JSON.stringify(getState().wishlist.wishlist));
+    return data;
+}

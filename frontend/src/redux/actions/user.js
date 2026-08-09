b2df70f717e30fc54ea71269e4e807e0ba8de920
Asma-Ehsan <asmaehsan212@gmail.com>
2026-08-09 22:40:48 +0500

    fix the path of  updateUserInformation

--- frontend/src/redux/actions/user.js
+++ frontend/src/redux/actions/user.js
@@ -34,7 +34,7 @@ export const updateUserInformation = (email, password, phoneNumber, name) => asy
   try {
     dispatch({ type: "UpdateUserInfoRequest" });
 
-    const { data } = await axios.get(`${server}/user.update-user-info`, {
+    const { data } = await axios.get(`${server}/user/update-user-info`, {
       email,
       password,
       phoneNumber,

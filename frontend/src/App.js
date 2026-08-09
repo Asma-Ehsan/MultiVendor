cfdf1a5df7244d4c77d330ed3a6ed0a51724e450
Asma-Ehsan <asmaehsan212@gmail.com>
2026-08-09 19:17:46 +0500

    changed path of ProductDetailsPage from /:name to /:id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ const App = () => {
           element={<ActivationPage />}
         />
         <Route path="/products" element={<ProductsPage/>}/>
-        <Route path="/product/:name" element={<ProductDetailsPage/>}/>
+        <Route path="/product/:id" element={<ProductDetailsPage/>}/>
         <Route path="/best-selling" element={<BestSellingPage/>}/>
         <Route path="/events" element={<EventsPage/>}/>
         <Route path="/faq" element={<FAQPage/>}/>

a1a00bcbbe8d24aed44069377d0e0c505d380126
Asma-Ehsan <asmaehsan212@gmail.com>
2026-08-24 14:51:56 +0500

    added add to cart functionality with stock validation and user notifications

--- frontend/src/components/Events/EventCard.jsx
+++ frontend/src/components/Events/EventCard.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
 import styles from '../../styles/styles'
 import CountDown from "./CountDown"
 import { backend_url } from '../../server'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { addToCart } from '../../redux/actions/cart'
 
 const EventCard = ({active, data}) => {
+  const {cart} = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (data) => {
+    const isItemExist = cart && cart.find((i) => i._id === data._id);
+
+    if(isItemExist){
+      toast.error("Item already in cart!");
+    }else{
+      if(data.stock < 1){
+        toast.error("Product stock limited!");
+      }else{
+        const cartData = {...data, qty: 1};
+        dispatch(addToCart(cartData));
+        toast.success("Item added to cart successfully!");
+      }
+    }
+  }
+
   return (
     <div className={`w-full block bg-white rounded-lg ${active ? "unset" : "mb-12"} lg:flex p-2`}>
       <div className="w-full lg:w-[50%] m-auto">
         <img src= {`${backend_url}uploads/${data?.images[0]}`} alt="" />
       </div>
-      <div className="w-full lg:[w-50%] px-10 flex flex-col justify-center">
+      <div className="w-full lg:[w-50%] py-5 px-10 flex flex-col justify-center">
         <h2 className= {`${styles.productTitle}`}> {data?.name} </h2>
         <p> {data.description}</p>
         <div className="flex py-2 justify-between">
@@ -18,11 +40,18 @@ const EventCard = ({active, data}) => {
                 <h5 className='font-bold text-[20px] text-[#333] font-Roboto'>{data.discountPrice} $</h5>
             </div>
             <span className='pr-3 font-[400] text-[17px] text-[#44a55e]'>
-              {data.sold_out}
+              {data.sold_out} sold
             </span>
         </div>
 
         <CountDown data = {data} />
+        <br/>
+        <div className="flex items-center">
+          <Link to = {`/product/${data._id}?isEvent=true;`}>
+          <div className={`${styles.button} text-[#fff]`} >See Details</div>
+          </Link>
+          <div className={`${styles.button} text-[#fff] ml-5`} onClick={() => addToCartHandler(data)} > Add to cart</div>
+        </div>
       </div>
     </div>
   )

5c1a5aed827c81c44d9aa347f76aa45aa5da04c5
Asma-Ehsan <asmaehsan212@gmail.com>
2026-08-25 23:50:35 +0500

    feat: enhance Checkout component with ShippingInfo and CartData integration

--- frontend/src/components/Checkout/Checkout.jsx
+++ frontend/src/components/Checkout/Checkout.jsx
@@ -1,11 +1,74 @@
-import React from 'react'
+import CartData from "./Checkout Components/CartData";
+import ShippingInfo from "./Checkout Components/ShippingInfo";
+import styles from "../../styles/styles";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
 
 const Checkout = () => {
-  return (
-    <div>
-      Checkout
-    </div>
-  )
-}
+  const { user } = useSelector((state) => state.user);
+  const { cart } = useSelector((state) => state.cart);
 
-export default Checkout
+  const [country, setCountry] = useState("");
+  const [city, setCity] = useState("");
+  const [userInfo, setUserInfo] = useState(false);
+  const [address1, setAddress1] = useState("");
+  const [address2, setAddress2] = useState("");
+  const [zipCode, setZipCode] = useState(null);
+  const [couponCode, setCouponCode] = useState("");
+  const [couponCodeData, setCouponCodeData] = useState(null);
+  const [discountPrice, setDiscountPrice] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const paymentSubmit = () => {
+    navigate("/payment");
+  };
+
+  return (
+    <div className="w-full flex flex-col items-center py-8">
+      <div className="w-[90%] 1000px:w-[70%] block 800px:flex">
+        <div className="w-full 800px:w-[65%]">
+          <ShippingInfo
+            user={user}
+            country={country}
+            setCountry={setCountry}
+            city={city}
+            setCity={setCity}
+            userInfo={userInfo}
+            setUserInfo={setUserInfo}
+            address1={address1}
+            setAddress1={setAddress1}
+            address2={address2}
+            setAddress2={setAddress2}
+            zipCode={zipCode}
+            setZipCode={setZipCode}
+          />
+        </div>
+        <div className="w-full 800px:w-[35%] 800px:mt-0 mt-8">
+          <CartData
+          // handleSubmit={handleSubmit}
+            // totalPrice={totalPrice}
+            // shipping={shipping}
+            // subTotalPrice={subTotalPrice}
+            couponCode={couponCode}
+            setCouponCode={setCouponCode}
+            // discountPercentenge={discountPercentenge}
+          />
+        </div>
+      </div>
+      <div
+        className={`${styles.button} w-[150px] 800px:w-[280px] mt-10`}
+        onClick={paymentSubmit}
+      >
+        <h5 className="text-white">Go to Payment</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;

24c45363341fb53c765575c7ebafbb0bd0e74d73
Asma-Ehsan <asmaehsan212@gmail.com>
2026-08-25 23:50:55 +0500

    feat: add CartData component for displaying cart summary and coupon code input

---
+++ frontend/src/components/Checkout/Checkout Components/CartData.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styles from '../../../styles/styles'
+
+const CartData = ({couponCode,setCouponCode}) => {
+  return (
+     <div className="w-full bg-[#fff] rounded-md p-5 pb-8">
+      <div className="flex justify-between">
+        <h3 className="text-[16px] font-[400] text-[#000000a4]">subtotal:</h3>
+        <h5 className="text-[18px] font-[600]">$ 5400</h5>
+      </div>
+      <br />
+      <div className="flex justify-between">
+        <h3 className="text-[16px] font-[400] text-[#000000a4]">shipping:</h3>
+        <h5 className="text-[18px] font-[600]">$ 500</h5>
+      </div>
+      <br />
+      <div className="flex justify-between border-b pb-3">
+        <h3 className="text-[16px] font-[400] text-[#000000a4]">Discount:</h3>
+        <h5 className="text-[18px] font-[600]">
+          23
+        </h5>
+      </div>
+      <h5 className="text-[18px] font-[600] text-end pt-3">$ 2300</h5>
+      <br />
+      <form>
+        <input
+          type="text"
+          className={`${styles.input} h-[40px] pl-2`}
+          placeholder="Coupoun code"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+          required
+        />
+        <input
+          className={`w-full h-[40px] border border-[#f63b60] text-center text-[#f63b60] rounded-[3px] mt-8 cursor-pointer`}
+          required
+          value="Apply code"
+          type="submit"
+        />
+      </form>
+    </div>
+  )
+}
+
+export default CartData

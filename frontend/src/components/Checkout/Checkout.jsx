3525a221be9e0610ca5f4fd7db9c4aa4bc30f44e
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-01 09:15:30 +0500

    feat: enhance payment submission with address validation and local storage update

--- frontend/src/components/Checkout/Checkout.jsx
+++ frontend/src/components/Checkout/Checkout.jsx
@@ -18,15 +18,43 @@ const Checkout = () => {
   const [zipCode, setZipCode] = useState(null);
   const [couponCode, setCouponCode] = useState("");
   const [couponCodeData, setCouponCodeData] = useState(null);
-  const [discountPrice, setDiscountPrice] = useState(null);
+    const [discountPrice, setDiscountPrice] = useState(null)
   const navigate = useNavigate();
+
+   const subTotalPrice = cart.reduce((acc,item) => acc + item.qty * item.discountPrice, 0);
+  const shipping = subTotalPrice * 0.1;
+    const discountPercentage = couponCodeData ? discountPrice : "";
+    const totalPrice = couponCodeData ? (subTotalPrice + shipping - discountPercentage).toFixed(2) : (subTotalPrice + shipping).toFixed(2);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
   const paymentSubmit = () => {
+    if(address1 === "" || address2 === "" || zipCode === null || country === "" || city === ""){
+      toast.error("Please choose your delivery address!")
+    }else{
+      const shippingAddress = {
+      address1, 
+      address2,
+      zipCode,
+      country,
+      city,
+    }
+    const orderData = {
+      cart,
+      totalPrice,
+      subTotalPrice,
+      shipping,
+      discountPrice,
+      shippingAddress,
+      user,
+    }
+
+    //update local storage with updated orders array
+    localStorage.setItem("latestOrder", JSON.stringify(orderData));
     navigate("/payment");
+    }
   };
 
   return (
@@ -51,14 +79,16 @@ const Checkout = () => {
         </div>
         <div className="w-full 800px:w-[35%] 800px:mt-0 mt-8">
           <CartData
-          // handleSubmit={handleSubmit}
-            // totalPrice={totalPrice}
+            totalPrice={totalPrice}
             couponCode={couponCode}
             setCouponCode={setCouponCode}
-            // discountPercentenge={discountPercentenge}
+            discountPercentage={discountPercentage}
             couponCodeData = {couponCodeData}
+            setDiscountPrice = {setDiscountPrice}
             setCouponCodeData = {setCouponCodeData}
             cart = {cart}
+            subTotalPrice = {subTotalPrice}
+            shipping = {shipping}
           />
         </div>
       </div>

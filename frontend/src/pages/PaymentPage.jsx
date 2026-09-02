c0f5bdadec6d5025b808b095d36641d39cd1383c
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-02 08:00:43 +0500

    add scroll to top functionality on PaymentPage component

--- frontend/src/pages/PaymentPage.jsx
+++ frontend/src/pages/PaymentPage.jsx
@@ -2,8 +2,13 @@ import Header from '../components/Layout/Header'
 import CheckoutSteps from '../components/Checkout/CheckoutSteps'
 import Footer from '../components/Layout/Footer'
 import Payment from "../components/Payment/Payment"
+import { useEffect } from 'react'
 
 const PaymentPage = () => {
+  useEffect(() => {
+    window.scrollTo(0,0);
+  },[]);
+  
   return (
     <div>
         <Header/>

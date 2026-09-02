3a030830ff0079f43b59e38346b4de8208eebf09
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-02 08:00:16 +0500

    refactor Payment component to manage order data and improve layout

--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import CartData from "./CartData/CartData";
+import PaymentInfo from "./PaymentInfo/PaymentInfo";
 
 const Payment = () => {
-  return (
-    <div>Payment</div>
-  )
-}
+  const [orderData, setOrderData] = useState([]);
 
-export default Payment
+  useEffect(() => {
+    const orderData = JSON.parse(localStorage.getItem("latestOrder"));
+    setOrderData(orderData);
+  },[]);
+  return (
+    <div className="w-full flex flex-col items-center py-8">
+      <div className="w-[90%] 1000px:w-[70%] block 800px:flex">
+        <div className="w-full 800px:w-[65%]">
+          <PaymentInfo />
+        </div>
+        <div className="w-full 800px:w-[35%] 800px:mt-0 mt-8">
+          <CartData orderData = {orderData} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;

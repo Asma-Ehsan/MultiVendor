f8437bf0ab70256e93396d474ceb234c58e5b737
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-02 08:18:01 +0500

    create payment controller for processing payments and retrieving Stripe API key

---
+++ backend/controller/payment.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const catchAsyncError = require("../middleware/catchAsyncError");
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+router.post("/payment/process", catchAsyncError(async (req, res, next) => {
+    const myPayment = await stripe.paymentIntents.create({
+      amount: req.body.amount,
+      currency: "inr",
+      metadata: {
+        company: "E-shop",
+      },
+    });
+    res.status(201).json({
+      success: true,
+      client_secret: myPayment.client_secret,
+    });
+  }),
+);
+
+router.get("/stripeapikey", catchAsyncError(async(req,res,next) => {
+    res.status(200).json({
+        stripeApikey: process.env.STRIPE_API_KEY,
+    })
+}))

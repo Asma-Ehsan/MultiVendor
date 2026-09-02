9ef856489a396b9fcb7563c5a19b7abedde2fca2
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-02 06:59:48 +0500

    created payment page

---
+++ frontend/src/pages/PaymentPage.jsx
@@ -0,0 +1,21 @@
+import Header from '../components/Layout/Header'
+import CheckoutSteps from '../components/Checkout/CheckoutSteps'
+import Footer from '../components/Layout/Footer'
+import Payment from "../components/Payment/Payment"
+
+const PaymentPage = () => {
+  return (
+    <div>
+        <Header/>
+        <br />
+        <br />
+        <CheckoutSteps active = {2}/>
+        <Payment/>
+        <br />
+        <br />
+        <Footer/>
+    </div>
+  )
+}
+
+export default PaymentPage

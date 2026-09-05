e5b7d22b8fc82a72e984088cc91421c3356a2477
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-05 16:24:59 +0500

    added stripe payment functionality

--- frontend/src/components/Payment/Payment.jsx
+++ frontend/src/components/Payment/Payment.jsx
@@ -1,22 +1,121 @@
 import { useEffect, useState } from "react";
 import CartData from "./CartData/CartData";
 import PaymentInfo from "./PaymentInfo/PaymentInfo";
+import {
+  CardNumberElement,
+  CardCvcElement,
+  CardExpiryElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { server } from "../../server.js";
+import { toast } from "react-toastify";
 
 const Payment = () => {
   const [orderData, setOrderData] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const stripe = useStripe();
+  const elements = useElements();
 
   useEffect(() => {
     const orderData = JSON.parse(localStorage.getItem("latestOrder"));
     setOrderData(orderData);
-  },[]);
+  }, []);
+
+  const createOrder = (data, actions) => {
+    //
+  };
+
+  const onApprove = async (data, actions) => {
+    //
+  };
+
+  const paypalPaymentHandler = async (paymentInfo) => {
+    //
+  };
+
+  const paymentData = {
+    amount: Math.round(orderData?.totalPrice * 100),
+  };
+
+  const order = {
+    cart: orderData?.cart,
+    shippingAddress: orderData?.shippingAddress,
+    user: user && user,
+    totalPrice: orderData?.totalPrice,
+  };
+
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const { data } = await axios.post(
+        `${server}/payment/process`,
+        paymentData,
+        config,
+      );
+
+      const client_secret = data.client_secret;
+      if(!stripe || !elements) return;
+      const result = await stripe.confirmCardPayment(client_secret, {
+        payment_method: {
+          card: elements.getElement(CardNumberElement),
+        },
+      });
+      if(result.error){
+        toast.error(result.error.message);
+      }else{
+        if(result.paymentIntent.status === "succeeded"){
+          order.paymentInfo = {
+            id: result.paymentIntent.id,
+            status: result.paymentIntent.status,
+            type: "Credit Card"
+          }
+          await axios.post(`${server}/order/create-order`, order, config).then((res) => {
+            setOpen(false);
+            navigate("/order/success");
+            toast.success("Order successful!");
+            localStorage.setItem("cartItems", JSON.stringify([]));
+            localStorage.setItem("latestOrder", JSON.stringify([]));
+          })
+        }
+      }
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+
+  const cashOnDeliveryHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="w-full flex flex-col items-center py-8">
       <div className="w-[90%] 1000px:w-[70%] block 800px:flex">
         <div className="w-full 800px:w-[65%]">
-          <PaymentInfo />
+          <PaymentInfo
+            user={user}
+            open={open}
+            setOpen={setOpen}
+            onApprove={onApprove}
+            createOrder={createOrder}
+            paymentHandler={paymentHandler}
+            cashOnDeliveryHandler={cashOnDeliveryHandler}
+          />
         </div>
         <div className="w-full 800px:w-[35%] 800px:mt-0 mt-8">
-          <CartData orderData = {orderData} />
+          <CartData orderData={orderData} />
         </div>
       </div>
     </div>

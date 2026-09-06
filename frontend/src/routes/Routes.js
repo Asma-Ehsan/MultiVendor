3cdd69e705c7509016800b95a280e9675c3ec2b6
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-06 19:07:47 +0500

    added order Details Page for user

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -1,19 +1,20 @@
-import LoginPage from "../pages/LoginPage.jsx"
-import SignupPage from "../pages/SignupPage.jsx"
-import ActivationPage from "../pages/ActivationPage.jsx"
-import HomePage from "../pages/HomePage.jsx"
-import ProductsPage from "../pages/ProductsPage.jsx"
-import BestSellingPage from "../pages/BestSellingPage.jsx"
-import EventsPage from "../pages/EventsPage.jsx"
-import FAQPage from "../pages/FAQPage.jsx"
-import ProductDetailsPage  from "../pages/ProductDetailsPage.jsx"
-import ProfilePage  from "../pages/ProfilePage.jsx"
-import ShopCreatePage from "../pages/ShopCreatePage.jsx"
-import SellerActivationPage from "../pages/SellerActivationPage.jsx"
-import ShopLoginPage from "../pages/ShopLoginPage.jsx"
+import LoginPage from "../pages/LoginPage"
+import SignupPage from "../pages/SignupPage"
+import ActivationPage from "../pages/ActivationPage"
+import HomePage from "../pages/HomePage"
+import ProductsPage from "../pages/ProductsPage"
+import BestSellingPage from "../pages/BestSellingPage"
+import EventsPage from "../pages/EventsPage"
+import FAQPage from "../pages/FAQPage"
+import ProductDetailsPage  from "../pages/ProductDetailsPage"
+import ProfilePage  from "../pages/ProfilePage"
+import ShopCreatePage from "../pages/ShopCreatePage"
+import SellerActivationPage from "../pages/SellerActivationPage"
+import ShopLoginPage from "../pages/ShopLoginPage"
 import CheckoutPage from "../pages/CheckoutPage"
 import PaymentPage from "../pages/PaymentPage"
 import OrderSuccessPage from "../pages/OrderSuccessPage"
+import OrderDetailsPage from "../pages/OrderDetailsPage"
 
 
 export {
@@ -33,4 +34,5 @@ export {
     CheckoutPage,
     PaymentPage,
     OrderSuccessPage,
+    OrderDetailsPage,
 }

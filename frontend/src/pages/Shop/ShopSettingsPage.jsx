74e4c97e8b43a9b556fa5d953f679e08a0e8d8b9
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-08 16:25:24 +0500

    update the ui of shop settings page

--- frontend/src/pages/Shop/ShopSettingsPage.jsx
+++ frontend/src/pages/Shop/ShopSettingsPage.jsx
@@ -1,15 +1,19 @@
-import Header from '../../components/Layout/Header'
-import Footer from '../../components/Layout/Footer'
-import ShopSettings from '../../components/Shop/ShopSettings'
+import ShopSettings from "../../components/Shop/ShopSettings";
+import DashboardHeader from "../../components/Shop/Layout/DashboardHeader";
+import DashboardSideBar from "../../components/Shop/Layout/DashboardSideBar";
 
 const ShopSettingsPage = () => {
   return (
     <div>
-        <Header/>
-        <ShopSettings/>
-        <Footer/>
+      <DashboardHeader />
+      <div className="flex items-start justify-between w-full">
+        <div className="w-[80px] 800px:w-[330px]">
+          <DashboardSideBar active={11} />
+        </div>
+        <ShopSettings />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ShopSettingsPage
+export default ShopSettingsPage;

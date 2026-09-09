56c3e1ea7640d41daeef58491c145df9fdab8ebe
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-09 15:20:27 +0500

    add routes to create a new message

---
+++ backend/controller/message.js
@@ -0,0 +1,40 @@
+const Messages = require("../model/messages");
+const ErrorHandler = require("../utils/ErrorHandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const express = require("express");
+const router = express.Router();
+
+
+// create new message
+router.post("/create-new-message",upload.array("images"), catchAsyncErrors(async(req, res, next) => {
+    try {
+        const messageData = req.body;
+
+        if(req.files){
+            const files = req.files;
+            const imageUrls = files.map((file) => `${file.fileName}`);
+            messageData.images = imageUrls;
+        }
+       messageData.conversationId = req.body.conversationId;
+       messageData.sender = req.body.sender;
+
+       const message = new Messages({
+        conversationId: messageData.conversationId,
+        sender: messageData.sender,
+        images: messageData.images ? messageData.images : undefined,
+       });
+
+       await message.save();
+
+       response.status(201).json({
+        success: true,
+        message,
+       })
+
+    } catch (error) {
+        return next(new ErrorHandler(error.response.message, 500));
+    }
+}));
+
+
+module.exports = router;

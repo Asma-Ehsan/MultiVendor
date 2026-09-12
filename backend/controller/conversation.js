d25a2ad1960034724fd53827d8679db3d2a8eb30
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-12 14:52:35 +0500

    created route to update last message

--- backend/controller/conversation.js
+++ backend/controller/conversation.js
@@ -3,7 +3,7 @@ const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncError");
 const express = require("express");
 const conversation = require("../model/conversation");
-const { isSeller } = require("../middleware/auth");
+const { isSeller, isAuthenticated } = require("../middleware/auth");
 const router = express.Router();
 
 //create a new conversation
@@ -51,6 +51,40 @@ router.get("/get-all-conversation-seller/:id", isSeller, catchAsyncErrors(async(
     } catch (error) {
         return next(new ErrorHandler(error.response.message, 500));
     }
-}))
+}));
+
+//update last message
+router.put("/update-last-message/:id", catchAsyncErrors(async(req, res, next) => {
+    try {
+        const {lastMessage, lastMessageId} = req.body;
+        const conversation = await Conversation.findByIdAndUpdate(req.params.id,{
+            lastMessage,
+            lastMessageId,
+        });
+        res.status(201).json({
+            success: true,
+            conversation,
+        })
+    } catch (error) {
+         return next(new ErrorHandler(error.message, 500));
+    }
+}));
+
+//get user conversation
+router.get("/get-all-conversation-user/:id", isAuthenticated, catchAsyncErrors(async(req, res, next) => {
+    try {
+        const conversations = await Conversation.find({members: {
+            $in: [req.params.id],
+        }}).sort({updatedAt: -1, createdAt: -1});
+
+        res.status(201).json({
+            success: true,
+            conversations
+        });
+
+    } catch (error) {
+        return next(new ErrorHandler(error.response.message, 500));
+    }
+}));
 
 module.exports = router;

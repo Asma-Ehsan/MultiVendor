87602a6abbf4b2e246f8f97ca8319a31894aada5
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-12 14:58:53 +0500

    updated useEffect to sort bestDeals according to max sold out products

--- frontend/src/components/Route/BestDeals/BestDeals.jsx
+++ frontend/src/components/Route/BestDeals/BestDeals.jsx
@@ -8,13 +8,10 @@ const BestDeals = () => {
   const { allProducts } = useSelector((state) => state.products);
 
   useEffect(() => {
-    // const d = products  && products.sort((a,b) => b.sold_out - a.sold_out);
-    const firstFive = allProducts?.slice(0,5); //will show only 5 items
+    const allProductsData = allProducts ? [...allProducts] : [];
+    const sortedData = allProductsData?.sort((a, b) => b.sold_out - a.sold_out);
+    const firstFive = sortedData && sortedData.slice(0, 5);
     setData(firstFive);
-    // const allProductsData = allProducts ? [...allProducts] : [];
-    // const sortedData = allProductsData?.sort((a, b) => b.sold_out - a.sold_out);
-    // const firstFive = sortedData && sortedData.slice(0, 5);
-    // setData(firstFive);
   }, [allProducts]);
 
   return (

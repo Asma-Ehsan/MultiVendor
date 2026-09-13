e636e45ed8d799461db3e6ee52fe3d859d164389
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-14 00:33:40 +0500

    refactor: update server and backend URL to use environment variables

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,10 @@
 // export const server = "http://localhost:8000/api/v2";
 // export const backend_url = "http://localhost:8000/";
 
-export const server = "https://multi-vendor-two-xi.vercel.app/api/v2";
-export const backend_url = "https://multi-vendor-two-xi.vercel.app/";
+// export const server = "https://multi-vendor-two-xi.vercel.app/api/v2";
+// export const backend_url = "https://multi-vendor-two-xi.vercel.app/";
+
+export const server =
+  process.env.REACT_APP_SERVER || "http://localhost:8000/api/v2";
+export const backend_url =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:8000/";

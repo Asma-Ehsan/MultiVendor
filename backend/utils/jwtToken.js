d511919d5fbce8da75b59f9ca5d705ca06811fd9
Asma-Ehsan <asmaehsan212@gmail.com>
2026-09-14 00:33:27 +0500

    refactor: update cookie options for production environment

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -2,12 +2,14 @@
 const sendToken = (user, statusCode, res) => {
     const token = user.getJwtToken();
 
+    const isProduction = process.env.NODE_ENV === "production";
+
     //options for cookies
     const options = {
         expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
         httpOnly: true,
-        sameSite: "none",
-        secure: true,
+        sameSite: isProduction ? "none" : "lax",
+        secure: isProduction,
         //This is a security option. It means: JavaScript running in the browser cannot access this cookie.
     };
 
